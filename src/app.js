6b36410e757990d567faf0486f9090d3122debe7
Danny Arnold <danny@dannyarnold.com>
2017-11-12 04:40:55 +0000

    Updated date picker

--- src/app.js
+++ src/app.js
@@ -17,14 +17,14 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-store.dispatch(addExpense({description:'Water Bill',amount:2200,}));
-store.dispatch(addExpense({description:'Gas Bill',amount:8200,}));
-store.dispatch(addExpense({description:'Rent',amount:182200,}));
+// store.dispatch(addExpense({description:'Water Bill',amount:2200,}));
+// store.dispatch(addExpense({description:'Gas Bill',amount:8200,}));
+// store.dispatch(addExpense({description:'Rent',amount:182200,}));
 
-store.dispatch(setTextFilter('gas'));
-setTimeout(() => {
-    store.dispatch(setTextFilter('bill'));
-},5000);
+// store.dispatch(setTextFilter('gas'));
+// setTimeout(() => {
+//     store.dispatch(setTextFilter('bill'));
+// },5000);
 // const state = store.getState();
 // const visibleExpenses = getVisableExpenses(state.expenses,state.filters);
 // console.log(visibleExpenses);
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,31 +1,61 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {setTextFilter,sortByAmount,sortByDate} from './../actions/filters';
+import moment from 'moment';
+import {DateRangePicker} from 'react-dates';
+import {setTextFilter,sortByAmount,sortByDate,setStartDate,setEndDate} from './../actions/filters';
 
-const ExpenseListFilters = (props) => (
-    <div>
-        <input type="text" value={props.filters.text} onChange={(e) => {
-            props.dispatch(setTextFilter(e.target.value));
-        }}/>
-        <select 
-            value = {props.filters.sortBy}        
-            onChange={(e) => {
-                if (e.target.value === 'date'){
-                    props.dispatch(sortByDate());  
-                }
-                else if (e.target.value === 'amount'){
-                    props.dispatch(sortByAmount());
-                }  
-            }}
-        >
-            <option value="date">Date    </option>
-            <option value="amount">Amount  </option>
-        </select>
-    </div>
-)
+class ExpenseListFilters extends React.Component {
+    state = {
+        callendarFocused:null
+    };
+    onFocusChange = (callendarFocused) => {
+        this.setState({callendarFocused});
+    };
+    onDatesChange = ({startDate,endDate}) => {
+        console.log(startDate,endDate);
+        this.props.dispatch(setStartDate(startDate));
+        this.props.dispatch(setEndDate(endDate));
+    };
+    
+    render(){  
+        return (  
+            <div>
+                <input type="text" value={this.props.filters.text} onChange={(e) => {
+                    this.props.dispatch(setTextFilter(e.target.value));
+                }}/>
+                <select 
+                    value = {this.props.filters.sortBy}        
+                    onChange={(e) => {
+                        if (e.target.value === 'date'){
+                            this.props.dispatch(sortByDate());  
+                        }
+                        else if (e.target.value === 'amount'){
+                            this.props.dispatch(sortByAmount());
+                        }  
+                    }}
+                >
+                    <option value="date">Date    </option>
+                    <option value="amount">Amount  </option>
+                </select>&nbsp;
+                <DateRangePicker 
+                    startDate={this.props.filters.startDate}
+                    endDate={this.props.filters.endDate}
+                    onDatesChange={this.onDatesChange}
+                    focusedInput={this.state.callendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    displayFormat="Do MMM YYYY"
+                    numberOfMonths={1}
+                    isOutsideRange={(day) => false}
+                    showClearDates={true}
+                />
+            </div>
+        );
+    };
+}
 const mapStateToProps = (state) => {
-   return {
+    return {
         filters: state.filters
-   };
+    };
 };
+
 export default connect(mapStateToProps)(ExpenseListFilters);
