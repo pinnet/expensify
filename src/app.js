6b51d1a9ae568ed59f67b88d715e034262ab3447
Danny Arnold <danny@dannyarnold.com>
2017-11-12 04:44:35 +0000

    tidy

--- src/app.js
+++ src/app.js
@@ -17,25 +17,11 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-// store.dispatch(addExpense({description:'Water Bill',amount:2200,}));
-// store.dispatch(addExpense({description:'Gas Bill',amount:8200,}));
-// store.dispatch(addExpense({description:'Rent',amount:182200,}));
-
-// store.dispatch(setTextFilter('gas'));
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'));
-// },5000);
-// const state = store.getState();
-// const visibleExpenses = getVisableExpenses(state.expenses,state.filters);
-// console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
 );
-
-
 ReactDOM.render(jsx,document.getElementById('app'));
 
 
