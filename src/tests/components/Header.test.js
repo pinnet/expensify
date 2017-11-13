cf3e7dba9b9cd144702082cb0f5b26f966c5c2b5
Danny Arnold <danny@dannyarnold.com>
2017-11-13 18:08:16 +0000

    setup jest

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,13 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react';
+import { shallow } from 'enzyme'; 
 import Header from './../../components/Header';
 
 it('should render correctly', () => {
-  const renderer =  new ReactShallowRenderer();
-     renderer.render(<Header/>);
-        expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('h1').length).toBe(1);
+
+
+  // const renderer =  new ReactShallowRenderer();
+  //    renderer.render(<Header/>);
+  //       expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
