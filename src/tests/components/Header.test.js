2cd1369d9be5a62258fca9d7d6dd14ce857b10c6
Danny Arnold <danny@dannyarnold.com>
2017-11-13 18:23:23 +0000

    setup Jest and enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme'; 
+import toJSON from 'enzyme-to-json';
 import Header from './../../components/Header';
 
 it('should render correctly', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').length).toBe(1);
-
-
+    expect(wrapper).toMatchSnapshot();
+  // expect(wrapper.find('h1').text()).toBe('Expensify');
   // const renderer =  new ReactShallowRenderer();
   //    renderer.render(<Header/>);
-  //       expect(renderer.getRenderOutput()).toMatchSnapshot();
+  //      
 });
