a83a148fe18965a08e57bfb6a0b2bd0b0f731e45
Danny Arnold <danny@dannyarnold.com>
2017-11-14 06:34:43 +0000

    added node server

--- src/app.js
+++ src/app.js
@@ -18,7 +18,6 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-console.log('test');
 const jsx = (
     <Provider store={store}>
         <AppRouter />
