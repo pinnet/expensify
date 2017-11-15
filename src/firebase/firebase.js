19c857ed3006cbb2e810856460d2f4a8056baf7a
Danny Arnold <danny@dannyarnold.com>
2017-11-15 03:32:15 +0000

    setup firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyBIViAxds4Fv_9lqqEwWdB34y_3avblaFs",
+    authDomain: "expensify-71a42.firebaseapp.com",
+    databaseURL: "https://expensify-71a42.firebaseio.com",
+    projectId: "expensify-71a42",
+    storageBucket: "expensify-71a42.appspot.com",
+    messagingSenderId: "91843540819"
+  };
+firebase.initializeApp(config);
+
+firebase.database().ref().set({
+    name: 'danny'
+})
