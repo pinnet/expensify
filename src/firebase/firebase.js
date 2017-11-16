9a720941cd07555d3d7d0644327e97ea662744ac
Danny Arnold <danny@dannyarnold.com>
2017-11-16 12:29:00 +0000

    setting up enviroment var.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 import expenses from './../tests/fixtures/firebase.expenses';
 var config = {
-    apiKey: "AIzaSyBIViAxds4Fv_9lqqEwWdB34y_3avblaFs",
-    authDomain: "expensify-71a42.firebaseapp.com",
-    databaseURL: "https://expensify-71a42.firebaseio.com",
-    projectId: "expensify-71a42",
-    storageBucket: "expensify-71a42.appspot.com",
-    messagingSenderId: "91843540819"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 firebase.initializeApp(config);
 const db = firebase.database();
