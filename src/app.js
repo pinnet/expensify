ce77f9af2ac6abd48cf2e8121921b3d42f23d9bb
Danny Arnold <danny@dannyarnold.com>
2017-11-17 08:39:07 +0000

    login and private Route established

--- src/app.js
+++ src/app.js
@@ -36,8 +36,9 @@ const renderApp = () => {
 ReactDOM.render(<p>Loading...</p>,document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
+
     if (user) {
-        store.dispatch(login())
+        store.dispatch(login(user.uid))
         store.dispatch(startSetExpenses()).then(() => {
             renderApp();
             if(history.location.pathname === '/'){
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,22 +7,22 @@ import CreateExpense from '../components/CreateExpense';
 import EditExpense from '../components/EditExpense';
 import Help from '../components/HelpPage';
 import Login from '../components/Login';
+import ErrorPage from '../components/ErrorPage';
 import { Provider } from 'react-redux';
 import createBrowserHistory from 'history/createBrowserHistory';
-
+import PrivateRoute from './PrivateRoute';
 export const history = createBrowserHistory();
 
 const AppRouter = () => (
     <Router history={history}>
     <div>
-        <Header/>
          <Switch>
             <Route path="/" component={Login} exact={true}/>
-            <Route path="/dashboard" component={Dashboard}  />
-            <Route path="/create" component={CreateExpense} />
-            <Route path="/edit/:id" component={EditExpense} />
+            <PrivateRoute path="/dashboard" component={Dashboard}  />
+            <PrivateRoute path="/create" component={CreateExpense} />
+            <PrivateRoute path="/edit/:id" component={EditExpense} />
             <Route path="/help" component={Help} />
-            <Route component={Help} />
+            <Route component={ErrorPage} />
         </Switch>
     </div>
     </Router>
